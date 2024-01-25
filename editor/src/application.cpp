35c2db4ff8061b13210dc651755ef5bb049ad123
jeang <qi-naught@gmail.com>
2024-01-25 18:54:00 -0500

    Added renderer, engine and editor folders. Added application and renderer class. Adjusted CMakeLists.

---
+++ editor/src/application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jeang on 2024-01-25.
+//
+
+#include "application.h"
---
+++ renderer/include/renderer.h
@@ -0,0 +1,14 @@
+//
+// Created by jeang on 2024-01-25.
+//
+
+#ifndef GRAPHICS_PLAYGROUND_RENDERER_H
+#define GRAPHICS_PLAYGROUND_RENDERER_H
+
+
+class renderer
+{
+};
+
+
+#endif//GRAPHICS_PLAYGROUND_RENDERER_H
